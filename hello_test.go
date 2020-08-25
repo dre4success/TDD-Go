e73c1f58b66092924c608b8d97a1e34afb691489
dre4success <dre4success@live.com>
2020-08-25 17:10:15 +0100

    update

--- hello_test.go
+++ hello_test.go
@@ -12,15 +12,36 @@ func TestHello(t *testing.T)  {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 	
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func (t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func (t *testing.T) {
+		got := Hello("Coutilard", "French")
+		want := "Bonjour, Coutilard"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Yoruba", func (t *testing.T) {
+		got := Hello("Wole", "Yoruba")
+		want := "Bawoni, Wole"
 
 		assertCorrectMessage(t, got, want)
 	})
